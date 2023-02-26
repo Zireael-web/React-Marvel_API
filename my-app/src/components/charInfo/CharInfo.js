e9f47c5993097b6d4de37a512657e31e21cafd5e
Ivan <Ivan.Vasilenko.01234@mail.ru>
2023-02-26 18:13:06 +0600

    1st commit

---
+++ my-app/src/components/charInfo/CharInfo.js
@@ -0,0 +1,128 @@
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/errorMessage';
+import Skeleton from '../skeleton/Skeleton'
+
+import './charInfo.scss';
+
+class CharInfo extends Component {
+    state = {
+        char: null,
+        loading: false,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.charId !== prevProps.charId) {
+            this.updateChar();
+        }
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    updateChar = () => {
+        const {charId} = this.props;
+        if (!charId) {
+            return;
+        }
+
+        this.onCharLoading();
+
+        this.marvelService
+            .getCharacter(charId)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    render() {
+        const {char, loading, error} = this.state;
+
+        const skeleton = !(loading || error || char) ? <Skeleton/> : null; 
+        const spinner = loading ? <Spinner/> : null;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const content = !(loading || error || !char) ? <View char={char}/> : null;
+
+        return (
+            <div className="char__info">
+                {skeleton}
+                {spinner}
+                {errorMessage}
+                {content}
+            </div>
+        )
+    }
+}
+
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+    const imgStyle = (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') ? {objectFit: 'unset'} : null
+
+    return (
+        <>
+            <div className="char__basics">
+                    <img src={thumbnail} alt={name} style={imgStyle}/>
+                    <div>
+                        <div className="char__info-name">{name}</div>
+                        <div className="char__btns">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+            </div>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {(comics.length === 0) ? 'There is no data about comics with the character' : null}
+                {comics.map((item, i) => {
+                        // eslint-disable-next-line
+                        if (i > 9) return; 
+                        return (
+                            <li key={i} className="char__comics-item">
+                                {item.name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </>
+    )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
+
+export default CharInfo;
---
+++ my-app/src/components/spinner/spinner.js
@@ -0,0 +1,57 @@
+const Spinner = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={{margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto'}} width="230px" height="230px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            <g transform="rotate(0 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-1.5027322404371584s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(30 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-1.366120218579235s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(60 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-1.2295081967213115s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(90 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-1.0928961748633879s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(120 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.9562841530054644s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(150 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.819672131147541s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(180 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.6830601092896175s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(210 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.5464480874316939s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(240 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.4098360655737705s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(270 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.27322404371584696s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(300 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="-0.13661202185792348s" repeatCount="indefinite"></animate>
+            </rect>
+            </g><g transform="rotate(330 50 50)">
+            <rect x="47" y="26" rx="3" ry="4" width="6" height="8" fill="#1121cc">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1.639344262295082s" begin="0s" repeatCount="indefinite"></animate>
+            </rect>
+            </g>
+        </svg>
+    )
+}
+
+export default Spinner;
